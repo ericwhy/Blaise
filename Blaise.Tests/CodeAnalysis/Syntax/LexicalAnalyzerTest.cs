a7ace0d4a0415e34d8b6d18939bee467588da26e
ericwhy <ericy@koretech.com>
2020-03-29 14:27:57 -0700

    Added additional unit tests to cover binary operators

--- Blaise.Tests/CodeAnalysis/Syntax/LexicalAnalyzerTest.cs
+++ Blaise.Tests/CodeAnalysis/Syntax/LexicalAnalyzerTest.cs
@@ -29,8 +29,106 @@ namespace Blaise.Tests.CodeAnalysis.Syntax
             Assert.Equal(tokens[1].Kind, secondKind);
             Assert.Equal(tokens[1].Text, secondText);
         }
-        public static IEnumerable<object[]> TokensData => from t in Tokens
-                                                          select new object[] { t.kind, t.text };
+        [Theory]
+        [MemberData(nameof(GetTokenPairsWithSeparatorsData))]
+        public void Lexer_Analyzes_TokenPairWithSeparator(SyntaxKind firstKind, string firstText,
+                                                          SyntaxKind secondKind, string secondText,
+                                                          SyntaxKind separatorKind, string separatorText)
+        {
+            var text = firstText + separatorText + secondText;
+            var tokens = SyntaxTree.ParseTokens(text).ToArray();
+            Assert.Equal(3, tokens.Length);
+            Assert.Equal(tokens[0].Kind, firstKind);
+            Assert.Equal(tokens[0].Text, firstText);
+            Assert.Equal(tokens[1].Kind, separatorKind);
+            Assert.Equal(tokens[1].Text, separatorText);
+            Assert.Equal(tokens[2].Kind, secondKind);
+            Assert.Equal(tokens[2].Text, secondText);
+        }
+        public static IEnumerable<object[]> TokensData => (from t in Tokens
+                                                           select new object[] { t.kind, t.text })
+                                                          .Union
+                                                          (from t in Separators
+                                                           select new object[] { t.kind, t.text });
+        public static IEnumerable<object[]> GetTokenPairsData =>
+            from tokenPair in GetTokenPairs()
+            select new object[] { tokenPair.firstKind, tokenPair.firstText, tokenPair.secondKind, tokenPair.secondText };
+        public static IEnumerable<object[]> GetTokenPairsWithSeparatorsData =>
+            from tokenPair in GetTokenPairsWithSeparators()
+            select new object[] { tokenPair.firstKind, tokenPair.firstText, tokenPair.secondKind, tokenPair.secondText, tokenPair.separatorKind, tokenPair.separatorText };
+        private static IEnumerable<(SyntaxKind firstKind, string firstText, SyntaxKind secondKind, string secondText)> GetTokenPairs()
+        {
+            foreach (var firstToken in Tokens)
+            {
+                foreach (var secondToken in Tokens)
+                {
+                    if (!RequiresSeparator(firstToken.kind, secondToken.kind))
+                    {
+                        yield return (firstToken.kind, firstToken.text, secondToken.kind, secondToken.text);
+                    }
+                }
+            }
+        }
+        private static IEnumerable<(SyntaxKind firstKind, string firstText,
+                                    SyntaxKind secondKind, string secondText,
+                                    SyntaxKind separatorKind, string separatorText)>
+                                    GetTokenPairsWithSeparators()
+        {
+            foreach (var firstToken in Tokens)
+            {
+                foreach (var secondToken in Tokens)
+                {
+                    if (RequiresSeparator(firstToken.kind, secondToken.kind))
+                    {
+                        foreach (var separatorToken in Separators)
+                        {
+                            yield return (firstToken.kind, firstToken.text, secondToken.kind, secondToken.text, separatorToken.kind, separatorToken.text);
+                        }
+                    }
+                }
+            }
+        }
+        private static IEnumerable<(SyntaxKind kind, string text)> Tokens => new[]
+        {
+            (SyntaxKind.IntegerToken, "1"),
+            (SyntaxKind.IntegerToken, "12"),
+            (SyntaxKind.IntegerToken, "123"),
+
+            (SyntaxKind.PlusToken, "+"),
+            (SyntaxKind.MinusToken, "-"),
+            (SyntaxKind.SplatToken, "*"),
+            (SyntaxKind.SlashToken, "/"),
+            (SyntaxKind.BangToken, "!"),
+            (SyntaxKind.ColonEqualsToken, ":="),
+            (SyntaxKind.ColonToken, ":"),
+            (SyntaxKind.AmpersandAmpersandToken, "&&"),
+            (SyntaxKind.PipePipeToken, "||"),
+            (SyntaxKind.EqualsToken, "="),
+            (SyntaxKind.LtGtToken, "<>"),
+            (SyntaxKind.BangEqualsToken, "!="),
+            (SyntaxKind.OpenParensToken, "("),
+            (SyntaxKind.CloseParensToken, ")"),
+            (SyntaxKind.LiteralAndToken, "and"),
+            (SyntaxKind.LiteralOrToken, "or"),
+            (SyntaxKind.LiteralNotToken, "not"),
+
+            (SyntaxKind.IdentifierToken, "a"),
+            (SyntaxKind.IdentifierToken, "cat"),
+            (SyntaxKind.IdentifierToken, "dog"),
+
+            (SyntaxKind.TrueKeyword, "true"),
+            (SyntaxKind.FalseKeyword, "false"),
+        };
+        private static IEnumerable<(SyntaxKind kind, string text)> Separators => new[]
+        {
+            (SyntaxKind.WhitespaceToken, " "),
+            (SyntaxKind.WhitespaceToken, "  "),
+            (SyntaxKind.WhitespaceToken, "\r"),
+            (SyntaxKind.WhitespaceToken, "\n"),
+            (SyntaxKind.WhitespaceToken, "\r\n"),
+            (SyntaxKind.WhitespaceToken, "\t"),
+            (SyntaxKind.WhitespaceToken, "\t\r\n"),
+        };
         private static bool RequiresSeparator(SyntaxKind firstKind, SyntaxKind secondKind)
         {
             var firstKindIsKeyword = firstKind.ToString().EndsWith("Keyword");
@@ -64,61 +162,5 @@ namespace Blaise.Tests.CodeAnalysis.Syntax
                 return true;
             return false;
         }
-        public static IEnumerable<object[]> GetTokenPairsData =>
-            from tokenPair in GetTokenPairs()
-            select new object[] { tokenPair.firstKind, tokenPair.firstText, tokenPair.secondKind, tokenPair.secondText };
-        private static IEnumerable<(SyntaxKind firstKind, string firstText, SyntaxKind secondKind, string secondText)> GetTokenPairs()
-        {
-            foreach (var firstToken in Tokens)
-            {
-                foreach (var secondToken in Tokens)
-                {
-                    if (!RequiresSeparator(firstToken.kind, secondToken.kind))
-                    {
-                        yield return (firstToken.kind, firstToken.text, secondToken.kind, secondToken.text);
-                    }
-                }
-            }
-        }
-        private static IEnumerable<(SyntaxKind kind, string text)> Tokens => new[]
-        {
-            // (SyntaxKind.WhitespaceToken, " "),
-            // (SyntaxKind.WhitespaceToken, "  "),
-            // (SyntaxKind.WhitespaceToken, "\r"),
-            // (SyntaxKind.WhitespaceToken, "\n"),
-            // (SyntaxKind.WhitespaceToken, "\r\n"),
-            // (SyntaxKind.WhitespaceToken, "\t"),
-            // (SyntaxKind.WhitespaceToken, "\t\r\n"),
-
-            (SyntaxKind.IntegerToken, "1"),
-            (SyntaxKind.IntegerToken, "12"),
-            (SyntaxKind.IntegerToken, "123"),
-
-            (SyntaxKind.PlusToken, "+"),
-            (SyntaxKind.MinusToken, "-"),
-            (SyntaxKind.SplatToken, "*"),
-            (SyntaxKind.SlashToken, "/"),
-            (SyntaxKind.BangToken, "!"),
-            (SyntaxKind.ColonEqualsToken, ":="),
-            (SyntaxKind.ColonToken, ":"),
-            (SyntaxKind.AmpersandAmpersandToken, "&&"),
-            (SyntaxKind.PipePipeToken, "||"),
-            (SyntaxKind.EqualsToken, "="),
-            (SyntaxKind.LtGtToken, "<>"),
-            (SyntaxKind.BangEqualsToken, "!="),
-            (SyntaxKind.OpenParensToken, "("),
-            (SyntaxKind.CloseParensToken, ")"),
-            (SyntaxKind.LiteralAndToken, "and"),
-            (SyntaxKind.LiteralOrToken, "or"),
-            (SyntaxKind.LiteralNotToken, "not"),
-
-            (SyntaxKind.IdentifierToken, "a"),
-            (SyntaxKind.IdentifierToken, "cat"),
-            (SyntaxKind.IdentifierToken, "dog"),
-
-            (SyntaxKind.TrueKeyword, "true"),
-            (SyntaxKind.FalseKeyword, "false"),
-        };
-
     }
 }
